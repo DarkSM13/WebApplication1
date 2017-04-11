5fe3fc6332efbc37536e458ad677641e08018711
B50 <B50@DESKTOP-BMFNQUS>
2017-04-11 21:58:13 +0200

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult StartChat(string nazwa)
+        {
+            Session["uzytkownik"] = nazwa;
+            return View("Chat");
+        }
+        public ActionResult Chat(string msg)
+        {
+            Message message = new Message()
+            {
+            Nazwa = Session["uzytkownik"] as String,
+            Czas = DateTime.Now,
+            Content = msg
+        };
+            return PartialView("Message", message);
+        }
+    }
+}
---
+++ WebApplication1/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Message
+    {
+        public string Nazwa { get; set; }
+
+        public DateTime Czas { get; set; }
+
+        public string Content { get; set; }
+    }
+}
